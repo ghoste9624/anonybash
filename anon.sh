a61092a5184e8e1624aeb15ae587966ec89d6cc6
ghoste9624 <jackspade44@outlook.com>
2025-04-15 08:35:00 -0400

    Update anon.sh

--- anon.sh
+++ anon.sh
@@ -12,6 +12,5 @@ echo "PS1='\e[3m\[\033[1;30m\]\D{%a-%b-%d-%Y}  \e[\033[38;5;214m\@
  \[\033[0;34m\][\[\033[0;36m\]\#\[\033[0;34m\]]\[\033[1;92m\] '" >> ~/.bashrc
 
 source ~/.bashrc
-termux-reload-settings 
 cd $HOME
 exit
