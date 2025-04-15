dd30454ca30f85b857de0965c84fe85835574eb0
ghoste9624 <jackspade44@outlook.com>
2025-04-15 08:32:07 -0400

    Update anon.sh

--- anon.sh
+++ anon.sh
@@ -1,7 +1,7 @@
 clear 
 
-echo -e "\e[3m\e[1;92mWE ARE ANONYMOUS..."
-
+echo -e "\e[3m\e[1;92mWE ARE ANONYMOUS...
+"
 rm -rf /data/data/com.termux/files/usr/etc/motd
 git clone https://github.com/ghoste9624/anonymous-motd /data/data/com.termux/files/usr/etc/motd
 echo "/data/data/com.termux/files/usr/etc/motd/init.sh" >> /data/data/com.termux/files/usr/etc/profile # or .zprofile if using zsh
@@ -12,5 +12,6 @@ echo "PS1='\e[3m\[\033[1;30m\]\D{%a-%b-%d-%Y}  \e[\033[38;5;214m\@
  \[\033[0;34m\][\[\033[0;36m\]\#\[\033[0;34m\]]\[\033[1;92m\] '" >> ~/.bashrc
 
 source ~/.bashrc
-
+termux-reload-settings 
+cd $HOME
 exit
