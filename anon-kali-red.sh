48d9e8a453b51e97897da1ad487e55ac8e5f5875
ghoste9624 <jackspade44@outlook.com>
2025-04-30 00:11:20 -0400

    Create anon-kali-red.sh

---
+++ anon-kali-red.sh
@@ -0,0 +1,9 @@
+echo "PS1='\[\033[0;94m\]┌──(\[\033[1;91m\]\u㉿kali\[\033[0;94m\])-[\[\033[0;1m\]\w\[\033[0;94m\]]
+\[\033[0;94m\]└─\[\033[0;91m\]#\[\033[0;1m\] '
+
+# enable color support of ls
+alias ls='ls --color=auto'
+alias dir='ls --color=auto --format=vertical'
+alias vdir='ls --color=auto --format=long'" >> ~/.bashrc
+source ~/.bashrc
+clear && exit
