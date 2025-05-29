15642ed5b4c8351ad6b2772e623aff6f9f365bf2
ghoste9624 <jackspade44@outlook.com>
2025-05-29 09:30:33 -0400

    Update anon-kali.sh

--- anon-kali.sh
+++ anon-kali.sh
@@ -1,12 +1,12 @@
 cd $SHOME
 rm -rf .bashrc
 echo "PS1='\[\033[0;94m\]┌──(\[\033[0;34m\]j4ck㉿kali\[\033[0;94m\])-[\[\033[1;96m\]\w\[\033[0;94m\]]
-\[\033[0;94m\]└─\[\033[0;34m\]#\[\033[1;97m\]
+\[\033[0;94m\]└─\[\033[0;34m\]#\[\033[1;97m\]'
 
 # enable color support of ls
 alias ls='ls --color=auto'
 alias dir='ls --color=auto --format=vertical'
-alias vdir='ls --color=auto --format=long'" > ~/.bashrc
+alias vdir='ls --color=auto --format=long" > ~/.bashrc
 source ~/.bashrc
 clear && exit
 
